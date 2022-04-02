67632b99ea88518119eb07de02e6b399ff608148
ddf <eef>
2022-04-02 12:26:05 +0800

    Implemented install and uninstall scripts and plist file

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-#git@github.com:paulbhart/toggleairport.git
-#originally from https://gist.github.com/albertbori/1798d88a93175b9da00b
+#!/usr/bin/env bash
+
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root, i.e. sudo $0"
     exit 1
 fi
 
-cp ./toggleAirport.sh /Library/Scripts/
-chmod 755 /Library/Scripts/toggleAirport.sh
-cp ./com.mine.toggleairport.plist /Library/LaunchAgents/
-chmod 600 /Library/LaunchAgents/com.mine.toggleairport.plist
-launchctl load /Library/LaunchAgents/com.mine.toggleairport.plist
+cp ./auto-connect-hotspot.sh /Library/Scripts/
+chmod 755 /Library/Scripts/auto-connect-hotspot.sh
+cp ./com.nick.auto-connect-hotspot.plist /Library/LaunchAgents/
+chmod 600 /Library/LaunchAgents/com.nick.auto-connect-hotspot.plist
+launchctl load /Library/LaunchAgents/com.nick.auto-connect-hotspot.plist
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#git@github.com:paulbhart/toggleairport.git
-#originally from https://gist.github.com/albertbori/1798d88a93175b9da00b
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root, i.e. sudo $0"
     exit 1
 fi
 
-rm /Library/Scripts/toggleAirport.sh
-rm  /Library/LaunchAgents/com.mine.toggleairport.plist
-launchctl unload /Library/LaunchAgents/com.mine.toggleairport.plist
+rm /Library/Scripts/auto-connect-hotspot.sh
+rm /Library/LaunchAgents/com.nick.auto-connect-hotspot.plist
+launchctl unload /Library/LaunchAgents/com.nick.auto-connect-hotspot.plist
